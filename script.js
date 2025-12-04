f8ab9b6eb9972e1e7ea869db12e66c1811b363e9
santialexis <santialexisfranqueira@gmail.com>
2025-12-04 13:11:25 -0300

    ajustes a mensajes

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 const claves = {
+                "inicio" : "pista a clave 1",
                 "clave 1" : "pista a clave 2",
                 "clave 2" : "pista a clave 3",
                 "clave 3" : "pista a clave 4",
                 "clave 4" : "pista a clave 5"
-            };
+            }
 
 const boton = document.getElementById("boton")
 const input = document.getElementById("clave")
 
-const pistaDiv = document.createElement("div");
-pistaDiv.id = "pista";
-document.body.appendChild(pistaDiv);
+/*div pistas*/
+const pistaDiv = document.createElement("div")
+pistaDiv.id = "pista"
+pistaDiv.textContent = claves["inicio"]
+document.body.appendChild(pistaDiv)
+
+/*div clave incorrecta*/
+const errorDiv = document.createElement("div")
+errorDiv.id = "error"
+errorDiv.style.color = "red"
+errorDiv.style.marginTop = "10px"
+errorDiv.hidden = true
+document.body.appendChild(errorDiv)
+
+/*div primera pista*/
+pistaDiv.textContent = claves["inicio"]
+pistaDiv.hidden = false
 
 boton.addEventListener("click", () => {
     const passwd = input.value.toLowerCase()
-    if(claves[passwd]){
-        pistaDiv.textContent = claves[passwd];
-        pistaDiv.hidden = false;
+    if (claves[passwd]) {
+        pistaDiv.textContent = claves[passwd]
+        errorDiv.hidden = true
     } else {
-        alert("Clave incorrecta");
-        pistaDiv.hidden = true;
+        errorDiv.textContent = "Error 404: Clave incorrecta"
+        errorDiv.hidden = false
     }
 })
