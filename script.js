2b501039b925a4953fcfd75b9b49507ad39803d2
santialexis <santialexisfranqueira@gmail.com>
2025-12-05 01:56:37 -0300

    agrego mensajes de inicio y fin

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const claves = {
                 "clave 2" : "pista: la clave es \"clave 3\"",
                 "clave 3" : "pista: la clave es \"clave 4\"",
                 "clave 4" : "pista: la clave es \"clave 5\"",
-                "clave 5" : "pista: la clave es \"clave 6\"",
-                "clave 6": "¡Felicidades! Has completado el juego."
+                "clave 5" : "pista: la clave es \"clave 6\""
             }
 
 const ordenClaves = ["clave 1", "clave 2", "clave 3", "clave 4","clave 5","clave 6"]
@@ -30,7 +29,18 @@ boton.addEventListener("click", () => {
     const passwd = input.value.toLowerCase().trim()
     if(passwd === ordenClaves[progreso]){ // Clave correcta en el orden esperado
         if (passwd === "clave 6") {
-            document.body.innerHTML = "<h1>¡Felicidades! Has completado el juego.</h1>";
+            document.body.innerHTML = `
+                <h1>¡Felicidades! Has completado el juego.</h1>
+                <p>
+                    Has demostrado tus habilidades en este desafío de búsqueda del tesoro en la UNLu. 
+                    Esperamos que hayas disfrutado de la aventura y aprendido algo nuevo en el camino.
+                </p>
+                <p>
+                    Por favor, regresa al puesto de donde comenzaste para recibir tu recompensa. 
+                    ¡Gracias por participar y sigue explorando el mundo de la informática!
+                </p>
+                <a href="https://www.unlu.edu.ar/carg-sistemas.html" target="_blank">Conoce más sobre la Licenciatura en Sistemas de Información</a>
+            `;
         } else {
             pistaDiv.textContent = claves[passwd];
             errorDiv.hidden = true;
